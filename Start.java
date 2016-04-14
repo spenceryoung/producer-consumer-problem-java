1c3e0af06ae0c763d54232ecb5a516164be96838
spenceryoung <syoung42788@gmail.com>
2016-04-13 19:37:03 -0500

    push

--- Start.java
+++ Start.java
@@ -3,7 +3,6 @@ package start;
 public class Start {
     public static void main(String[] args) {
         Buffer BUFFER = new Buffer();
-        StopWatch watch = new StopWatch(15000);
 
         Producer p = new Producer(BUFFER);
         Consumer c = new Consumer(BUFFER);
